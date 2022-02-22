795bb8b60f8ce0bd33257ad5c77f72befe1c27c1
Re-taro <aminokacho@gmail.com>
2022-02-23 02:24:30 +0900

    ✨ create mutation addFood

--- src/food/food.resolver.ts
+++ src/food/food.resolver.ts
@@ -1,13 +1,34 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Food } from './food.model';
 import { FoodService } from './food.service';
 
 @Resolver()
 export class FoodResolver {
   constructor(private readonly foodService: FoodService) {}
-
+  // TODO: これはデバッグ用です
   @Query(() => [Food], { name: 'foods' })
-  findAll() {
+  findAll(): Promise<Food[]> {
     return this.foodService.findAll();
+  }
+
+  @Mutation(() => Food, { name: 'addFood' })
+  addFood(
+    @Args('user_id') user_id: string,
+    @Args('recipe_title') recipe_title: string,
+    @Args('recipe_url') recipe_url: string,
+    @Args('image_url') image_url: string,
+    @Args('recipe_material') recipe_material: string,
+    @Args('recipe_indication') recipe_indication: string,
+    @Args('recipe_cost') recipe_cost: string,
+  ): Promise<Food> {
+    return this.foodService.addFood(
+      user_id,
+      recipe_title,
+      recipe_url,
+      image_url,
+      recipe_material,
+      recipe_indication,
+      recipe_cost,
+    );
   }
 }
--- src/food/food.service.ts
+++ src/food/food.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Food } from './food.model';
 
 @Injectable()
 export class FoodService {
   constructor(private readonly prisma: PrismaService) {}
-
-  findAll() {
+  // TODO: これはデバッグ用です
+  async findAll(): Promise<Food[]> {
     return this.prisma.food.findMany();
+  }
+
+  async addFood(
+    user_id: string,
+    recipe_title: string,
+    recipe_url: string,
+    image_url: string,
+    recipe_material: string,
+    recipe_indication: string,
+    recipe_cost: string,
+  ): Promise<Food> {
+    return this.prisma.food.create({
+      data: {
+        user_id,
+        recipe_title,
+        recipe_url,
+        image_url,
+        recipe_material,
+        recipe_indication,
+        recipe_cost,
+      },
+    });
   }
 }
