a03e1fc08676d9a0893b2dd48520cd8d9cbffb59
Re-taro <aminokacho@gmail.com>
2022-02-22 20:13:55 +0900

    ✨ update App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
+import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      // Resolverでexpressのreq/resを利用する場合設定する
+      // context: ({ req, res }): { req: Request; res: Response } => ({
+      //   req,
+      //   res,
+      // }),
+      // corsの設定が必要な場合
+      // cors: {
+      //   origin: process.env.ORIGINS?.split(','),
+      //   credentials: true,
+      // },
+      debug: process.env.NODE_ENV === 'production' ? false : true,
+      playground: process.env.NODE_ENV === 'production' ? false : true,
+    }),
+    PrismaModule,
+    FoodModule,
+  ],
 })
 export class AppModule {}
