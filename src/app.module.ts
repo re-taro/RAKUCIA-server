c557e04429d623c5c262311ee5d7c2cbed2236d9
Re-taro <aminokacho@gmail.com>
2022-02-22 13:13:33 +0900

    🔥 remove all files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeOrmConfigService } from './config/typeorm-config.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`.env/${process.env.NODE_ENV}.env`, '.env/default.env'],
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useClass: TypeOrmConfigService,
-    }),
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-  });
-  const configService = new ConfigService();
-  const sync = configService.get('DB_SYNC');
-  if (sync !== 'false') {
-    const swaggerOptions = new DocumentBuilder()
-      .setTitle('api document')
-      .setDescription('API仕様書')
-      .setVersion('1.0')
-      .build();
-    const document = SwaggerModule.createDocument(app, swaggerOptions);
-    SwaggerModule.setup('document', app, document);
-  }
-  const port = configService.get('API_PORT') || 3003;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
