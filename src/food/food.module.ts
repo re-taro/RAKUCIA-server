e1c7d2e4e1833c3ae54df3f1c9523013b9414db8
Re-taro <aminokacho@gmail.com>
2022-02-22 13:52:46 +0900

    🔥 remove files

--- src/food/food.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FoodController } from './food.controller';
-import { FoodService } from './food.service';
-
-@Module({
-  controllers: [FoodController],
-  providers: [FoodService],
-})
-export class FoodModule {}
