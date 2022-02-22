57a066c65151c916792e4d835ec8a4082561af08
Re-taro <aminokacho@gmail.com>
2022-02-23 01:00:04 +0900

    ✨ change the GraphQL setting to food module
    separate dependence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,7 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { PrismaModule } from './prisma/prisma.module';
 import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      // Resolverでexpressのreq/resを利用する場合設定する
-      // context: ({ req, res }): { req: Request; res: Response } => ({
-      //   req,
-      //   res,
-      // }),
-      // corsの設定が必要な場合
-      // cors: {
-      //   origin: process.env.ORIGINS?.split(','),
-      //   credentials: true,
-      // },
-      debug: process.env.NODE_ENV === 'production' ? false : true,
-      playground: process.env.NODE_ENV === 'production' ? false : true,
-    }),
-    PrismaModule,
-    FoodModule,
-  ],
+  imports: [FoodModule],
 })
 export class AppModule {}
--- src/food/food.module.ts
+++ src/food/food.module.ts
@@ -1,10 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FoodResolver } from './food.resolver';
 import { FoodService } from './food.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      // Resolverでexpressのreq/resを利用する場合設定する
+      // context: ({ req, res }): { req: Request; res: Response } => ({
+      //   req,
+      //   res,
+      // }),
+      // corsの設定が必要な場合
+      // cors: {
+      //   origin: process.env.ORIGINS?.split(','),
+      //   credentials: true,
+      // },
+      debug: process.env.NODE_ENV === 'production' ? false : true,
+      playground: process.env.NODE_ENV === 'production' ? false : true,
+    }),
+    PrismaModule,
+  ],
   providers: [FoodResolver, FoodService],
 })
 export class FoodModule {}
