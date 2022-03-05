0a17dd3d68a85286fec152efa18cf4153748ec8f
Re-taro <aminokacho@gmail.com>
2022-03-05 13:31:57 +0900

    ✨ change the modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const bodyParser = require('body-parser');
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { middleware as LineMiddleware } from '@line/bot-sdk';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { FoodModule } from './food/food.module';
 import { LinebotModule } from './linebot/linebot.module';
 import { LinebotController } from './linebot/linebot.controller';
@@ -10,6 +13,17 @@ import { LinebotConfigService } from './linebot/linebot.config.service';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      debug: process.env.NODE_ENV === 'production' ? false : true,
+      playground: process.env.NODE_ENV === 'production' ? false : true,
+      cors: {
+        origin: process.env.ORIGINS?.split(','),
+        credentials: true,
+      },
+    }),
     FoodModule,
     LinebotModule,
     ConfigModule.forRoot({
