79627596087c0f949e0b5d7904aa3ab0fef1827e
Re-taro <aminokacho@gmail.com>
2022-03-06 19:33:33 +0900

    ✨ update secrets

--- src/linebot/linebot.data.ts
+++ src/linebot/linebot.data.ts
@@ -18,7 +18,7 @@ export const richMenu: RichMenu = {
       },
       action: {
         type: 'uri',
-        uri: 'https://liff.line.me/1656947920-BZXEZp8D',
+        uri: process.env.LIFF_URL,
       },
     },
     {
